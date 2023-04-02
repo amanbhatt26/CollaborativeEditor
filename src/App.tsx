addbf779fa304f22d4c605aa46a89586a1a700d3
Aman Bhatt <amanb2704@gmail.com>
2023-04-02 20:15:27 +0530

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import { Editor } from "./Components/Editor";
 import { v4 as uuid } from "uuid";
 import randomColor from "randomcolor";
+import {Routes, Route, useParams} from "react-router-dom";
 
 export const App = () => {
   return (
     <div className="h-[100vh] w-[100vw]">
-      <Editor
+      <Routes>
+        <Route path="/" element={<div>Home Page</div>}/>
+        <Route path="/document/:id" element={<EditorPage/>}/>
+        <Route path="*" element={<div>Page Not Found</div>}/>
+      </Routes>
+     
+    </div>
+  );
+};
+
+const EditorPage = ()=>{
+  const {id} = useParams(); 
+  return <div className="h-[100vh] w-[100vw]">
+    <div className="h-wrap w-full"> Document {id}</div>
+    <Editor
         id={uuid()}
         roomName="room-1"
         editorName="editor-1"
@@ -15,6 +30,5 @@ export const App = () => {
           alpha: 1,
         })}
       />
-    </div>
-  );
-};
+  </div>
+}
